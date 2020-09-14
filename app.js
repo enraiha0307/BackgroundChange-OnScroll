6c9f8cf56a504a77101f503955a50d655c34817f
enraiha0307 <akankshagahlot0307@gmail.com>
2020-09-14 18:15:52 +0530

    initial files

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log('working');
+function bgChanger() {
+    if(this.scrollY > this.innerHeight/2){
+        document.body.classList.add('bg-active');
+    }else{
+        document.body.classList.remove('bg-active');
+    }
+    
+}
+window.addEventListener("scroll", bgChanger);
